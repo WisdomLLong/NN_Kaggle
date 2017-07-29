09445345d7dbfc260c6245e5f6ade117fc7b5d9a
WisdomLLong <f_l_wang@163.com>
2017-07-29 19:58:57 +0800

    单独写好grid, validation curve, learning curve函数会使代码很简洁

--- Titanic/Titanic_learn.py
+++ Titanic/Titanic_learn.py
@@ -220,15 +220,277 @@ for dataset in combine:
 train_df = train_df.drop(['FareBand'], axis=1)
 combine = [train_df, test_df]
 
+print(train_df.describe())
+#@@ 显示train_df各个列或者说特征的均值等基本信息的统计
+
+#correlation matrix
+f, ax = plt.subplots(figsize= (12, 9))
+sns.heatmap(train_df.corr(), vmax=.8, square=True)
+#@@ 画出特这值之间的相关性
 
 
+
+###############################################################################
+# Part II: Learning Model
+###############################################################################
     
+#------------------------------------------------------------------------------
+# Step 03: Learning model
+#------------------------------------------------------------------------------    
+
+# grid search
+def grid_search_model(X, Y, model, parameters, cv):
+    CV_model = GridSearchCV(estimator= model, param_grid=parameters, cv=cv)
+    CV_model.fit(X, Y)
+    CV_model.cv_results_
+    print('Best Score:', CV_model.best_score_, '/ Best parameters:',\
+           CV_model.best_params_)
     
+# validation curve
+def validation_curve_model(X, Y, model, param_name, parameters, cv, ylim, log=True):
+    #@@ 1、掉函数，检索超参数
+    train_scores, test_scores = validation_curve(model, X, Y, \
+    param_name=param_name, param_range=parameters, cv=cv, scoring='accuracy' )
+    #@@ 2、求出均值与标准差
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    #@@ 3、画图
+    plt.figure()
+    plt.title("Validation curve")
+    plt.fill_between(parameters, train_scores_mean - train_scores_std,\
+                     train_scores_mean + train_scores_std, alpha=0.1,color="r")
+    plt.fill_between(parameters, test_scores_mean - test_scores_std,\
+                     test_scores_mean + test_scores_std, alpha=0.1, color='g')
+    if log==True:
+        #@@ semilogx是在对x轴取对数
+        plt.semilogx(parameters, train_scores_mean, 'o-', color='r', label='Training score')
+        plt.semilogx(parameters, test_scores_mean, 'o-', color='g', label='Cross-validation score')
+    else:
+        plt.plot(parameters, train_scores_mean, 'o-', color='r', label='Training score')
+        plt.plot(parameters, test_scores_mean, '0-', color='g', label='Cross-validation score')
+        
+    #plt.ylim([0.55, 0.9])
+    if ylim is not None:
+        plt.ylim(*ylim)
+        
+    plt.ylabel('Score')
+    plt.xlabel('Parameter C')
+    plt.legend(loc='best')
+    return plt
     
+# Learning curve
+def Learning_curve_model(X, Y, model, cv, train_sizes):
+    plt.figure()
+    plt.title('Learning curve')
+    plt.xlabel('Training examples')
+    plt.ylabel('Score')
+        
+    train_sizes, train_scores, test_scores = learning_curve(model, X, Y, \
+            cv=cv, n_jobs=4, train_sizes=train_sizes)
+        
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+        
+    plt.fill_between(train_scores_mean - train_scores_std,\
+                     train_scores_mean + train_scores_std, alpha=0.1,color="r")
+    plt.fill_between(test_scores_mean - test_scores_std,\
+                     test_scores_mean + test_scores_std, alpha=0.1, color='g')
+    plt.plot(train_scores_mean, 'o-', color='r', label='Training score')
+    plt.plot(test_scores_mean, '0-', color='g', label='Cross-validation score')
+    #@@ legend放到最佳的位置
+    plt.legend(loc='best')
+    return plt
+        
+# Learning, predictiong and printing results
+def predict_model(X, Y, model, Xtest, submit_name):
+    model.fit(X, Y)
+    Y_pred = model.predict(Xtest)
+    score = cross_val_score(model, X, Y, cv=cv)
     
+    submission = pd.DataFrame({'PassengerId': test_df['PassengerId'], \
+                               'Survived': Y_pred})
+    #@@ 保存为CSV数据
+    submission.to_csv(submit_name, index=False)
+    return score
+
+###############################################################################
+# data and cv
+X_data = train_df.drop('Survived', axis=1)  #data:Features
+Y_data = train_df['Survived']   #data: Labels
+X_test_kaggle = test_df.drop('PassengerId', axis=1).copy()  #test data(Kaggle)
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+
+###############################################################################
+# Logistic Regression
+search_param = 0    # 1--grid search / 0--don't search
+plot_vc      = 0    # 1--display validation curve / 0--don't display
+plot_lc      = 1    # 1--display learning curve / 0--don't display
+
+# grid search: logistic Regression
+model = LogisticRegression()
+if search_param==1:
+    #@@ logspace(a,b,N) 把10的a次方到10的b次方区间分成N份
+    param_range = np.logspace(-6, 5, 12)
+    param_grid  = dict(C=param_range)
+    grid_search_model(X_data, Y_data, model, param_grid, cv)
     
+#Validation Curve: Logistic Regression
+if plot_vc==1:
+    param_range = np.logspace(-6, 3, 10)
+    param_name = 'C'
+    ylim=[0.55, 0.9]
+    validation_curve_model(X_data, Y_data, model, 'C', param_range, cv, ylim)
     
+#learn curve
+logreg = LogisticRegression(C=1000)   
+if plot_lc ==1:
+    train_size = np.linspace(.1, 1.0, 5)
+    #@@ 从0.1到1.0的15个等距离数，因为learning curve比较占内存，因此可以把数改小一些
+    Learning_curve_model(X_data, Y_data, logreg, cv, train_size)
     
+# Logistic Regression
+acc_log = predict_model(X_data, Y_data, logreg, X_test_kaggle, 'submission_Logistic.csv')
+
+
+###############################################################################
+# Support Vector Machines
+search_param = 0
+plot_vc      = 0
+plot_lc      = 0
+
+# grid searhc: SVM
+if search_param == 1:
+    param_range = np.linspace(0.5, 5, 9)
+    param_grid = dict(C=param_range)
+    grid_search_model(X_data, Y_data, SVC(), param_grid, cv)
+    
+# Validation Cureve: SVC
+if plot_vc == 1:
+    param_range = np.linspace(0.1, 10, 10)
+    param_name = 'C'
+    ylim = [0.78, 0.90]
+    validation_curve_model(X_data, Y_data, SVC(), 'C', param_range, cv, ylim, log=False)
+    
+# learn curve: SVC
+svc = SVC(C=1, probability=True)
+if plot_lc ==1:
+    train_size = np.linspace(.1, 1.0, 5)
+    Learning_curve_model(X_data, Y_data, svc, cv, train_size)
+    
+# Support Vector Machines
+acc_svc = predict_model(X_data, Y_data, svc, X_test_kaggle, 'submission_SVM.csv')
+
+    
+###############################################################################
+# KNN
+search_param = 0
+plot_vc      = 0
+plot_lc      = 0
+
+# grid search: KNN
+if search_param == 1:
+    param_range = (np.linspace(1, 10, 10)).astype(int)
+    param_grid = dict(n_neighbors = param_range)
+    grid_search_model(X_data, Y_data, KNeighborsClassifier(), param_grid, cv)
+    
+# Vlidation Curve: KNN
+if plot_vc == 1:
+    param_range = np.linspace(2, 20, 10).astype(int)
+    param_name = 'n_neighbors'
+    ylim = [0.75, 0.90]
+    validation_curve(X_data, Y_data, KNeighborsClassifier(),\
+                     'n_neighbors', param_range, cv, ylim, log = False)
+    
+# learn curve: KNN
+knn = KNeighborsClassifier(n_neighbors = 10)
+if plot_lc == 1:
+    train_size = np.linspace(.1, 1.0, 5)
+    Learning_curve_model(X_data, Y_data, knn, cv, train_size)
+
+# KNN
+acc_knn = predict_model(X_data, Y_data, knn, X_test_kaggle, 'submission_KNN.csv')
+
+
+###############################################################################
+# Naive Bayes
+# Gaussian Naive Bayes
+gaussian = GaussianNB()
+acc_gaussian = predict_model(X_data, Y_data, gaussian, X_test_kaggle,\
+                    'bmission_Gassian_Naive_Bayes.csv')
+
+
+###############################################################################
+# Perceptron
+perceptron = Perceptron()
+acc_perceptron = predict_model(X_data, Y_data, perceptron, X_test_kaggle,\
+                    'submission_Perception.csv')
+
+
+###############################################################################
+# Linear SVC
+linear_svc = LinearSVC()
+acc_linear_svc = predict_model(X_data, Y_data, linear_svc, X_test_kaggle,\
+                    'submission_Linear_SVC.csv')
+
+
+###############################################################################
+# Stochastic Gradient Descent
+sgd = SGDClassifier()
+acc_sgd = predict_model(X_data, Y_data, sgd, X_test_kaggle,'submission_sgd.csv')
+
+
+###############################################################################
+# Decision Tree
+decision_tree = DecisionTreeClassifier()
+acc_decision_tree = predict_model(X_data, Y_data, decision_tree, X_test_kaggle,\
+                     'submission_Decision_Tree.csv')
+
+
+###############################################################################
+# Random Forest
+search_param = 0
+plot_vc      = 0
+plot_lc      = 0
+#@@ 第一个if是在找参数min_samples_leaf的最优解
+if plot_vc == 1:
+    param_range = np.linspace(10, 110, 10).astype(int)
+    ylim = [0.75, 0.90]
+    validation_curve_model(X_data, Y_data, RandomForestClassifier(min_samples_leaf=12),\
+                'n_esti')
+if plot_vc == 1:
+    param_range = np.linspace(1, 21, 10).astype(int)
+    ylim = [0.75, 0.90]
+    validation_curve_model(X_data, Y_data, RandomForestClassifier(n_estimators=80),\
+                'min_samples_leaf', param_range, cv, ylim, log = False)
+    
+#Random Forest
+random_forest = RandomForestClassifier(n_estimators=80, random_state=0, min_samples_leaf=12)
+acc_random_forest = predict_model(X_data, Y_data, random_forest, X_test_kaggle, 'submission_random_forest.csv')
+
+
+
+###############################################################################
+# Ensemble voting
+#ensemble votring
+ensemble_voting = VotingClassifier(estimators=[('lg', logreg), ('sv', svc), ('rf', random_forest),('kn',knn)], voting='soft')
+acc_ensemble_voting = predict_model(X_data, Y_data, ensemble_voting, X_test_kaggle, 'submission_ensemble_voting.csv')
+
+models = pd.DataFrame({'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+                                'Random Forest', 'Naive Bayes', 'Perceptron',
+                                'Stochastic Gradient Decent', 'Linear SVC',
+                                'Decision Tree', 'ensemble_voting'],'KFoldScore': [acc_svc.mean(), acc_knn.mean(), acc_log.mean(),
+                                acc_random_forest.mean(), acc_gaussian.mean(), acc_perceptron.mean(),
+                                acc_sgd.mean(), acc_linear_svc.mean(), acc_decision_tree.mean(), acc_ensemble_voting.mean()],
+                                'Std': [acc_svc.std(), acc_knn.std(), acc_log.std(),
+                                acc_random_forest.std(), acc_gaussian.std(), acc_perceptron.std(),
+                                acc_sgd.std(), acc_linear_svc.std(), acc_decision_tree.std(), acc_ensemble_voting.std()]})
+
+models.sort_values(by='KFoldScore', ascending=False)
     
     
     
