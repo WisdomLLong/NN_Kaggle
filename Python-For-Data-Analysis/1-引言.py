4a9907e2df4c74f6d210697fe143421f9d74c611
WisdomLLong <f_l_wang@163.com>
2018-01-03 22:49:34 +0800

    Update 1-引言.py

--- Python-For-Data-Analysis/1-引言.py
+++ Python-For-Data-Analysis/1-引言.py
@@ -3,3 +3,14 @@
 import json
 path = 'E:/Job/MLanguage/Python/Workplace_DataAnalysis/pydata-book/datasets/bitly_usagov/example.txt'
 records = [json.loads(line) for line in open(path)]
+time_zones = [rec['tz'] for rec in records if 'tz' in rec]
+
+#@@ defaultdict （对dict的功能扩展，当key不存在时，放回一个默认值，而不是KeyError）
+from collections import defaultdict
+# 统计次数
+def get_count(sequence):
+  counts = defaultdict(int)
+  for x in sequence:
+    count[x] += 1
+  return count
+
