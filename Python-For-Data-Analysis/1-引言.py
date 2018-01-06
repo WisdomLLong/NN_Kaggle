87eb69c92965e5ea23ccaa91051ece3f9150c44c
WisdomLLong <f_l_wang@163.com>
2018-01-06 21:13:00 +0800

    Update 1-引言.py

--- Python-For-Data-Analysis/1-引言.py
+++ Python-For-Data-Analysis/1-引言.py
@@ -5,15 +5,37 @@ path = 'E:/Job/MLanguage/Python/Workplace_DataAnalysis/pydata-book/datasets/bitl
 records = [json.loads(line) for line in open(path)]
 time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 
+
 #@@ defaultdict （对dict的功能扩展，当key不存在时，放回一个默认值，而不是KeyError）
-from collections import defaultdict
+
 ###### 统计次数
+from collections import defaultdict
 def get_count(sequence):
   counts = defaultdict(int)
   for x in sequence:
     count[x] += 1
   return count
+counts = get_count(time_zones)
+
 ###### 对字典进行排序
 # 转化为list再排序
 def top_counts(countDict, n = 10):
+  countList = [(value, key) for key,value in countDict.item()]
+  countList.sort()
+  return countList[-n:]
+top_counts(counts)
+
+###### 利用库函数直接实现对字典的 统计+排序
+from collection import Counter
+counts = Counter(time_zones)
+counts.most_common(10)
+
+###### 利用pandas和numpy进行分析
+from pandas import DataFrame, Series
+import pandas as; import numpy as np
+frame = DataFrame(records)    # record 是一个List，其中每一个元素是一个字典
+frame['tz'][:10]    # 直接查看tz列的前10个数据，frame['tz']是一个Series对象
+tz_counts = frame['tz'].value_counts()    # Series对象有一个value_counts方法
+
+
   
